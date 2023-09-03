aced8e857cee09d27e86bc0c3b905aea0975c111
zsj <1814231678@qq.com>
2023-09-03 15:35:34 +0800

    second commit

--- src/main/java/com/zsj/gitgithub/GitTest.java
+++ src/main/java/com/zsj/gitgithub/GitTest.java
@@ -8,5 +8,6 @@ package com.zsj.gitgithub;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
